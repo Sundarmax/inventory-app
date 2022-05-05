7bb1d94c3fd48a7a3ffe7d41e9cd08f4464c4b58
sundar <sundar.info22@gmail.com>
2022-05-05 23:58:17 +0530

    inventory schema is updated

--- inventory/product/models.py
+++ inventory/product/models.py
@@ -1,3 +1,28 @@
+import imp
+from operator import mod
+from statistics import mode
+from unicodedata import category
 from django.db import models
+from django.utils import timezone
+from supplier.models import Supplier
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        pass
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=40)
+    product_description = models.CharField(max_length=40)
+    product_unit = models.CharField(max_length=40)
+    product_price = models.FloatField()
+    product_quantity = models.IntegerField()
+    product_status = models.IntegerField()
+    product_category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    product_supplier = models.ForeignKey(Supplier,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.product_name
