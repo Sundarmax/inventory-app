09e4e75114693970f6737bb55fcc53efaa742bb1
sundar <sundar.info22@gmail.com>
2022-05-09 21:51:27 +0530

    Django filter updates

--- inventory/product/urls.py
+++ inventory/product/urls.py
@@ -1,8 +1,9 @@
 import imp
 from django.urls import path
 
-from product.views import retrieveProducts
+from product.views import retrieveProducts, retriveProductsByCategory
 
 urlpatterns = [
     path('test',retrieveProducts ,name="get_products"),
+    path('category',retriveProductsByCategory,name="get_product_by_category")
 ]
